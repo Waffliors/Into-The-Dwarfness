022389f9ef0a01c7d88235cdf18863a0be5f2b1a
MatthewVicente <41347019+MatthewVicente@users.noreply.github.com>
2019-04-04 21:43:58 -0300

    Test

--- IntoTheDwarfness/src/intothedwarfness/IntoTheDwarfness.java
+++ IntoTheDwarfness/src/intothedwarfness/IntoTheDwarfness.java
@@ -16,6 +16,7 @@ public class IntoTheDwarfness {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
