779b643d98f72facb1cb5d766dbb86f3d6c7189b
RaphaelO-Melo <48573578+RaphaelO-Melo@users.noreply.github.com>
2019-04-22 10:48:42 -0300

    Commented code in main class
    Header and annotations added

--- IntoTheDwarfness/src/intothedwarfness/IntoTheDwarfness.java
+++ IntoTheDwarfness/src/intothedwarfness/IntoTheDwarfness.java
@@ -4,22 +4,22 @@
  * and open the template in the editor.
  */
 package intothedwarfness;
-
 import intothedwarfness.Classes.Window;
 
-/**
- * @Studio: Waffliors
- * @Devs: Matheus Vicente, Nathan Andre and Raphael Melo
- */
-public class IntoTheDwarfness {
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                             *                   
+ *                            @Studio: Waffliors                               *
+ *                             @Team: Bugnatron                                *
+ *            @Devs: Matheus Vicente, Nathan Andre and Raphael Melo            *
+ *                             GDD available at:                               *
+ *                                                                             *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-    /**
-     * @param args the command line arguments
-     */
-    
+public class IntoTheDwarfness {
     public static void main(String[] args) {
+        //Initializing game window:
         Window screen = new Window("Into The Dwarfness");
-        
+        //Starting GameState and the game loop:
         screen.init();
         screen.run();
     }
