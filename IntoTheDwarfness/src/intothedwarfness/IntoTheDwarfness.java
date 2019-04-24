32a951f3502636e4b3ba46ee11cbab8ecb32a318
DarkShadow038 <44512789+DarkShadow038@users.noreply.github.com>
2019-04-24 03:56:37 -0300

    Add files via upload

--- IntoTheDwarfness/src/intothedwarfness/IntoTheDwarfness.java
+++ IntoTheDwarfness/src/intothedwarfness/IntoTheDwarfness.java
@@ -6,52 +6,30 @@
 package intothedwarfness;
 
 import java.io.IOException;
+
 import intothedwarfness.Classes.Player;
-import intothedwarfness.Classes.Tile;
 import intothedwarfness.Classes.Tilemap;
 import intothedwarfness.Classes.Window;
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.ArrayList;
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                             *                   
  *                            @Studio: Waffliors                               *
  *                             @Team: Bugnatron                                *
- *            @Devs: Matheus Vicente, Nathan Andre and Raphael Melo            *
+ *            @Devs: Matheus Vicente, Nathan André and Raphael Melo            *
  *                             GDD available at:                               *
  *                                                                             *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-public class IntoTheDwarfness extends JFrame {
+public class IntoTheDwarfness {
     public static void main(String[] args) throws IOException {
-        ArrayList<ArrayList<BufferedImage>> animationslist= new ArrayList();
-        loadanimations(animationslist);
+        //Creating Player
+        Player player = new Player();
+        Tilemap tilemap = new Tilemap(0, 0, 0, 0, 0, 0, 0, 0);
         
-        
-        Player player = new Player(animationslist);
-        Tilemap tilemap = new Tilemap();
-        Window screen = new Window("Into The Dwarfness", player, tilemap);
-        
-        
-        ArrayList<Tile> tilelist = new ArrayList();
-        
+        //Initializing game window:
+        Window screen = new Window("Into The Dwarfness", player);
+        //Starting GameState and the game loop:
         screen.init();
         screen.run();
-    }
-    
-    private static void loadanimations(ArrayList<ArrayList<BufferedImage>> animations) throws IOException{
-        ArrayList<BufferedImage> dwarf = new ArrayList();
-        dwarf.add(ImageIO.read(new File("images/dwarf/idle_r_1.png")));
-        dwarf.add(ImageIO.read(new File("images/dwarf/idle_r_2.png")));
-        dwarf.add(ImageIO.read(new File("images/dwarf/idle_r_3.png")));
-        dwarf.add(ImageIO.read(new File("images/dwarf/idle_r_4.png")));
-        dwarf.add(ImageIO.read(new File("images/dwarf/idle_r_5.png")));
-        animations.add(dwarf);
-       
     }
 }
