d3c66ca443e576eb825341719b18fa4bb8ee55bc
RaphaelO-Melo <48573578+RaphaelO-Melo@users.noreply.github.com>
2019-06-08 17:18:56 -0300

    Song cleanup

--- IntoTheDwarfness/src/intothedwarfness/Classes/States/GameStateManager.java
+++ IntoTheDwarfness/src/intothedwarfness/Classes/States/GameStateManager.java
@@ -7,35 +7,36 @@
 package intothedwarfness.Classes.States;
 
 public class GameStateManager {
+
     public GameState currGameState;
-    
+
     /* *********************** Class Constructor **************************** */
     public GameStateManager() {
         //Set the initial gamestate
         currGameState = new PauseState();
     }
-    
+
     /* **************************Class Methods******************************* */
-    
     /**
      * Initialize the game state
      */
     public void init() {
         currGameState.init();
     }
-    
+
     /**
-     * @return the type of the state 
+     * @return the type of the state
      */
     public String getType() {
         return currGameState.getType();
     }
-    
+
     /**
      * Switch the state of the game
+     *
      * @param newState : the new state
      */
-    public void switchState (GameState newState) {
+    public void switchState(GameState newState) {
         currGameState = newState;
         currGameState.init();
     }
