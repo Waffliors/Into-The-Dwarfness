d0685d2b380a5927a48385ac110101f3ca10cfaf
RaphaelO-Melo <48573578+RaphaelO-Melo@users.noreply.github.com>
2019-06-08 16:30:01 -0300

    Collidable and Drawable cleanup

--- IntoTheDwarfness/src/intothedwarfness/Interfaces/Drawable.java
+++ IntoTheDwarfness/src/intothedwarfness/Interfaces/Drawable.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package intothedwarfness.Interfaces;
 
-import intothedwarfness.Classes.Map;
 import java.awt.Graphics;
+import intothedwarfness.Classes.Map;
 
 public interface Drawable{
     public void paintComponent(Graphics g);
