561e2b7b893d78a9680c7ba540ce7599a7b3be37
MatthewVicente <41347019+MatthewVicente@users.noreply.github.com>
2019-06-07 21:24:50 -0300

    Rebuild game when player dies

--- IntoTheDwarfness/src/intothedwarfness/Classes/HUD.java
+++ IntoTheDwarfness/src/intothedwarfness/Classes/HUD.java
@@ -15,15 +15,15 @@ import java.net.MalformedURLException;
 public class HUD implements Drawable {
 
     private final Player player;
-    private final ArrayList<BufferedImage> hud;
+    private final ArrayList<BufferedImage> health_bar;
     private String fName;
     private InputStream is;
     private Font font;
 
     //Loading the HUD images
-    public HUD(ArrayList<BufferedImage> HUD, Player player) {
+    public HUD(ArrayList<BufferedImage> healthBar, Player player) {
         this.player = player;
-        this.hud = HUD;        
+        this.health_bar = healthBar;        
         this.fName = "/Resources/RetroGaming.ttf";
         this.is = HUD.class.getResourceAsStream(fName);
 
@@ -37,11 +37,15 @@ public class HUD implements Drawable {
         }
     }
 
+    public ArrayList<BufferedImage> getHealthBar() {
+        return health_bar;
+    }
+    
     @Override
     public void paintComponent(Graphics g) {
-        g.drawImage(this.hud.get(player.getLife()), 27, 50, 27 + 125, 50 + 32, 0, 0, 125, 32, null);
-        g.drawImage(this.hud.get(7), 700, 50, 700 + 50, 50 + 50, 0, 0, 400, 400, null);
-        g.drawImage(this.hud.get(8), 850, 50, 850 + 50, 50 + 50, 0, 0, 400, 400, null);
+        g.drawImage(this.health_bar.get(player.getLife()), 27, 50, 27 + 125, 50 + 32, 0, 0, 125, 32, null);
+        g.drawImage(this.health_bar.get(7), 700, 50, 700 + 50, 50 + 50, 0, 0, 400, 400, null);
+        g.drawImage(this.health_bar.get(8), 850, 50, 850 + 50, 50 + 50, 0, 0, 400, 400, null);
 
         g.setColor(new Color(209, 209, 209));
         g.setFont(font.deriveFont(40.f));
@@ -49,15 +53,15 @@ public class HUD implements Drawable {
         g.drawString("" + player.getBossKilledCount(), 913, 85);
         
         if(player.getBossKilledCount()>=1){
-            BufferedImage image = this.hud.get(9).getSubimage(0,0,64, 64);
+            BufferedImage image = this.health_bar.get(9).getSubimage(0,0,64, 64);
             g.drawImage(image, 30, 100, 32, 32, null);
         }
     }
     
     public void paintPauseMenu(Graphics g) {
         
-        g.drawImage(this.hud.get(11), 0, 0, 1024, 768, null);
-        g.drawImage(this.hud.get(10), 0, 0, 1024, 768, null);        
+        g.drawImage(this.health_bar.get(11), 0, 0, 1024, 768, null);
+        g.drawImage(this.health_bar.get(10), 0, 0, 1024, 768, null);        
     }
 
     @Override
--- IntoTheDwarfness/src/intothedwarfness/Classes/Window.java
+++ IntoTheDwarfness/src/intothedwarfness/Classes/Window.java
@@ -3,7 +3,7 @@
  ** within this window will be created and drawn the objects of the game      **
  ** (Player, Enemy and Map), collected the events of the keyboard and where   **
  ** will be the game loop.                                                    **
- ******************************************************************************/
+ ***************************************************************************** */
 package intothedwarfness.Classes;
 
 import java.awt.Color;
@@ -28,27 +28,28 @@ import intothedwarfness.Classes.States.PauseState;
 import intothedwarfness.Classes.States.GameStateManager;
 
 public class Window extends JFrame implements KeyListener {
+
     // Constants
     private final Map MAP;
-    private final Player PLAYER;
     private final ArrayList<Song> SONGS;
-    private final ArrayList<Enemy> ENEMIES;
     private final int SCREEN_WIDTH, SCREEN_HEIGHT;
     private final ArrayList<BufferedImage> SPRITES;
-    private final HUD HUD;
     private boolean pause;
 
     // Variables of the class
+    private Player PLAYER;
+    private ArrayList<Enemy> ENEMIES;
+    private HUD HUD;
     private List<Node> path;
-    private int enemyCont;
     private GameStateManager gsm;
-    private final ArrayList<Drawable> DRAWABLES;
+    private ArrayList<Drawable> DRAWABLES;
 
-    /***
+    /**
+     * *
      * Game Screen Constructor
-     * 
+     *
      * @param SPRITES: List of sprites that will be used in the game
-     * @param SONGS:   List of songs that will be used in the game
+     * @param SONGS: List of songs that will be used in the game
      */
     public Window(ArrayList<BufferedImage> SPRITES, ArrayList<Song> SONGS, ArrayList<BufferedImage> health_bar) {
 
@@ -60,12 +61,10 @@ public class Window extends JFrame implements KeyListener {
         this.SCREEN_WIDTH = 1024;
         this.SCREEN_HEIGHT = 768;
         this.MAP = new Map(SPRITES.get(6), 12, 16, SPRITES.get(5));
-        this.PLAYER = new Player(SPRITES.get(0), SONGS, MAP);
-        this.ENEMIES = this.enemiesFactory();
-        this.HUD = new HUD(health_bar, this.PLAYER);
-        this.PLAYER.receiveCollidables(ENEMIES);
-        this.DRAWABLES = loadDrawables();
-        
+        this.ENEMIES = new ArrayList();
+        this.DRAWABLES = new ArrayList();
+        buildGame(health_bar);
+
         // Set configuration of the screen
         this.setSize(this.SCREEN_WIDTH, this.SCREEN_HEIGHT);
         this.setSize(this.SCREEN_WIDTH, this.SCREEN_HEIGHT);
@@ -83,9 +82,10 @@ public class Window extends JFrame implements KeyListener {
         return pause;
     }
 
-    /***
+    /**
+     * *
      * Load all drawable elements list
-     * 
+     *
      * @return : ArrayList that contains all drawable elements
      */
     private ArrayList<Drawable> loadDrawables() {
@@ -100,17 +100,19 @@ public class Window extends JFrame implements KeyListener {
         return elements;
     }
 
-    /***
+    /**
+     * *
      * Initialize the Game state manager
      */
     public void initialize() {
         gsm = new GameStateManager();
-        gsm.init();   
+        gsm.init();
     }
 
-    /***
+    /**
+     * *
      * Initialize all the enemies of the game
-     * 
+     *
      * @return : ArrayList that contains all game's enemies
      */
     private ArrayList<Enemy> enemiesFactory() {
@@ -118,41 +120,39 @@ public class Window extends JFrame implements KeyListener {
         // All the enemies will collide with the player
         ArrayList<Collidable> enemyCollidables = new ArrayList();
         enemyCollidables.add(PLAYER);
-        
-        
-        Enemy spider_1 = new Enemy(64*4, 64*9, 1, SPRITES.get(2), SONGS, MAP, 
+
+        Enemy spider_1 = new Enemy(64 * 4, 64 * 9, 1, SPRITES.get(2), SONGS, MAP,
                 enemyCollidables, 0);
-        Enemy spider_2 = new Enemy(64*2, 64*1, 2, SPRITES.get(2), SONGS, MAP, 
+        Enemy spider_2 = new Enemy(64 * 2, 64 * 1, 2, SPRITES.get(2), SONGS, MAP,
                 enemyCollidables, 0);
-        Enemy spider_3 = new Enemy(64*6, 64*8, 2, SPRITES.get(2), SONGS, MAP, 
+        Enemy spider_3 = new Enemy(64 * 6, 64 * 8, 2, SPRITES.get(2), SONGS, MAP,
                 enemyCollidables, 0);
-        
-        Enemy fire_elemental_1 = new Enemy(64*3, 64*3, 3, SPRITES.get(7), SONGS, MAP, 
+
+        Enemy fire_elemental_1 = new Enemy(64 * 3, 64 * 3, 3, SPRITES.get(7), SONGS, MAP,
                 enemyCollidables, 1);
-        Enemy fire_elemental_2 = new Enemy(64*9, 64*4, 3, SPRITES.get(7), SONGS, MAP, 
+        Enemy fire_elemental_2 = new Enemy(64 * 9, 64 * 4, 3, SPRITES.get(7), SONGS, MAP,
                 enemyCollidables, 1);
-        Enemy fire_elemental_3 = new Enemy(64*9, 64*2, 3, SPRITES.get(7), SONGS, MAP, 
+        Enemy fire_elemental_3 = new Enemy(64 * 9, 64 * 2, 3, SPRITES.get(7), SONGS, MAP,
                 enemyCollidables, 1);
-        Enemy fire_elemental_4 = new Enemy(64*12, 64*8, 3, SPRITES.get(7), SONGS, MAP, 
+        Enemy fire_elemental_4 = new Enemy(64 * 12, 64 * 8, 3, SPRITES.get(7), SONGS, MAP,
                 enemyCollidables, 1);
-       
-        
-        Enemy gladiator_1 = new Enemy(64*3, 64*4, 4, SPRITES.get(3), SONGS, MAP, 
+
+        Enemy gladiator_1 = new Enemy(64 * 3, 64 * 4, 4, SPRITES.get(3), SONGS, MAP,
                 enemyCollidables, 2);
-        Enemy gladiator_2 = new Enemy(64*12, 64*4,4, SPRITES.get(3), SONGS, MAP, 
+        Enemy gladiator_2 = new Enemy(64 * 12, 64 * 4, 4, SPRITES.get(3), SONGS, MAP,
                 enemyCollidables, 2);
-        Enemy gladiator_3 = new Enemy(64*8, 64*5, 5, SPRITES.get(3), SONGS, MAP, 
+        Enemy gladiator_3 = new Enemy(64 * 8, 64 * 5, 5, SPRITES.get(3), SONGS, MAP,
                 enemyCollidables, 2);
-        Enemy gladiator_4 = new Enemy(64*7, 64*8, 5, SPRITES.get(3), SONGS, MAP, 
+        Enemy gladiator_4 = new Enemy(64 * 7, 64 * 8, 5, SPRITES.get(3), SONGS, MAP,
                 enemyCollidables, 2);
-        
-        Enemy minotaur_1 = new Enemy(64*12, 64*4, 6, SPRITES.get(4), SONGS, MAP, 
+
+        Enemy minotaur_1 = new Enemy(64 * 12, 64 * 4, 6, SPRITES.get(4), SONGS, MAP,
                 enemyCollidables, 3);
-        Enemy minotaur_2 = new Enemy(64*4, 64*9, 8, SPRITES.get(4), SONGS, MAP, 
+        Enemy minotaur_2 = new Enemy(64 * 4, 64 * 9, 8, SPRITES.get(4), SONGS, MAP,
                 enemyCollidables, 3);
-        Enemy minotaur_3 = new Enemy(64*8, 64*9, 8, SPRITES.get(4), SONGS, MAP, 
+        Enemy minotaur_3 = new Enemy(64 * 8, 64 * 9, 8, SPRITES.get(4), SONGS, MAP,
                 enemyCollidables, 3);
-        
+
         enemies.add(spider_1);
         enemies.add(spider_2);
         enemies.add(spider_3);
@@ -165,12 +165,29 @@ public class Window extends JFrame implements KeyListener {
         enemies.add(gladiator_2);
         enemies.add(gladiator_3);
         enemies.add(gladiator_4);
-        
+
         enemies.add(minotaur_1);
         enemies.add(minotaur_2);
         enemies.add(minotaur_3);
-                
+
         return enemies;
+    }
+
+    private void buildGame(ArrayList<BufferedImage> health_bar) {
+        System.out.println(this.ENEMIES.isEmpty());
+        if (this.ENEMIES.size() > 0) {
+            this.ENEMIES.clear();
+        }
+        if (this.DRAWABLES.size() > 0) {
+            this.DRAWABLES.clear();
+        }
+        this.PLAYER = new Player(SPRITES.get(0), SONGS, MAP);
+        this.ENEMIES = this.enemiesFactory();
+        this.HUD = new HUD(health_bar, this.PLAYER);
+        this.PLAYER.receiveCollidables(ENEMIES);
+        this.DRAWABLES = loadDrawables();
+        
+        initialize();
     }
 
     // Game Loop
@@ -206,11 +223,13 @@ public class Window extends JFrame implements KeyListener {
             }
             if ("PlayState".equals(gsm.getType())) {
 
-                    int temp = 0;
-                    int temp2 = 0;
+                int temp = 0;
+                int temp2 = 0;
                 PLAYER.update();
                 PLAYER.receiveCollidables(ENEMIES);
-                
+                if (PLAYER.getLife() == 0) {
+                    buildGame(HUD.getHealthBar());
+                }
                 for (Enemy enemy : this.ENEMIES) {
                     if (enemy.isStage(this.MAP)) {
                         if (enemy.getXPosition() % 64 == 0 && enemy.getYPosition() % 64 == 0) {
@@ -226,14 +245,14 @@ public class Window extends JFrame implements KeyListener {
                     if (enemy.died()) {
                         temp = temp + 1;
                     }
-                    if (enemy.died() && "BossType".equals(enemy.getType())){
+                    if (enemy.died() && "BossType".equals(enemy.getType())) {
                         temp2 = temp2 + 1;
                         temp = temp - 1;
                     }
                     PLAYER.setEnemiesKilledCount(temp);
                     PLAYER.setBossKilledCount(temp2);
-                    
-                    if(PLAYER.getBossKilledCount() >= 1){
+
+                    if (PLAYER.getBossKilledCount() >= 1) {
                         MAP.setPortal(true);
                     }
                 }
@@ -268,12 +287,12 @@ public class Window extends JFrame implements KeyListener {
                 find = true;
             }
         }
-        
+
         this.path = AStar.aEstrela(enemy.getNodePos(), node, MAP);
         enemy.setPath(path);
 
     }
-    
+
     private void pathToPlayer(Enemy enemy) {
         Node end = PLAYER.getNodePos();
 ////        if (!PLAYER.getNodePos().getNeighbors().get(0).isBlocked()) {
@@ -281,9 +300,9 @@ public class Window extends JFrame implements KeyListener {
 ////        } else if (!PLAYER.getNodePos().getNeighbors().get(1).isBlocked()) {
 ////            end = PLAYER.getNodePos().getNeighbors().get(1);
 ////        }
-       this.path = AStar.aEstrela(enemy.getNodePos(), end, MAP);
+        this.path = AStar.aEstrela(enemy.getNodePos(), end, MAP);
         enemy.setPath(path);
-        if (path == null){
+        if (path == null) {
             setRandomPath(enemy);
         }
 
@@ -314,16 +333,6 @@ public class Window extends JFrame implements KeyListener {
     public void keyReleased(KeyEvent e) {
         PLAYER.setCurrentMove('.');
         PLAYER.setLook(e.getKeyChar());
-
-        //if (PLAYER.checkStage(e.getKeyChar()) > 0) {
-        if (PLAYER.getActualStage() > 0) {
-            for (Enemy enemy : this.ENEMIES) {
-                if (enemy.getActualStage() == this.MAP.getActualStage()) {
-                    this.MAP.getStageEnemies().add(enemy);
-                }
-            }
-            //PLAYER.initializeCollidables();
-        }
     }
 
     @Override
@@ -351,14 +360,10 @@ public class Window extends JFrame implements KeyListener {
                 if ("PauseState".equals(gsm.getType())) {
                     HUD.paintPauseMenu(graphics);
                 }
-                //graphics.drawImage(this.SPRITES.get(8), 0, 0, null);
                 // Disposes of this graphics context, it's no longer referenced.
                 graphics.dispose();
             } while (strategy.contentsRestored());
             strategy.show();
         } while (strategy.contentsLost());
-    }
-    private void setCollidables(Player PLAYER) {
-        
     }
 }
